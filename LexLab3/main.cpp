9b2ca699430b6f8ae5067d071888d64ac108fe7d
Tony Khamphouy <tkham1@unh.newhaven.edu>
2023-10-02 12:49:18 -0400

    Initial Commit

---
+++ LexLab3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LexLab3
+//
+//  Created by Tony Khamphouy on 10/2/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
